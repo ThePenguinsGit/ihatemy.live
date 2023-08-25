93ecb4105eaedf85dee2a4fa3b66db4230b3e104
LinusTebbe <linus@tebbe.dev>
2023-08-25 13:22:02 +0200

    add animated background

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full min-h-screen bg-[url('/img/background.png')] bg-repeat">
+  <div id="background" class="w-full min-h-screen bg-[url('/img/background.png')] bg-repeat">
     <div class="w-full bg-red-300 shadow-md py-4 px-2 flex flex-row">
       <nuxt-img src="/img/crab.png" sizes="sm:50px"/>
       <h1 class="text-[50px] ml-4 -mt-1">The Penguins</h1>
@@ -9,3 +9,33 @@
 </template>
 <script setup lang="ts">
 </script>
+
+<style scoped lang="scss">
+$bg-width: -264px;
+$bg-height: 141px;
+
+@-webkit-keyframes bg-scrolling {
+  0% { background-position: $bg-width $bg-height; }
+}
+@-moz-keyframes    bg-scrolling {
+  0% { background-position: $bg-width $bg-height; }
+}
+@-o-keyframes      bg-scrolling {
+  0% { background-position: $bg-width $bg-height; }
+}
+@keyframes         bg-scrolling {
+  0% { background-position: $bg-width $bg-height; }
+}
+
+/* Main styles */
+#background {
+  -webkit-animation: bg-scrolling 2s infinite;
+  -moz-animation:    bg-scrolling 2s infinite;
+  -o-animation:      bg-scrolling 2s infinite;
+  animation:         bg-scrolling 2s infinite;
+  -webkit-animation-timing-function: linear;
+  -moz-animation-timing-function:    linear;
+  -o-animation-timing-function:      linear;
+  animation-timing-function:         linear;
+}
+</style>
