f00d33c589f947e5946b508873142e09d14f8fd6
LinusTebbe <linus@tebbe.dev>
2023-08-25 13:24:22 +0200

    add stylehint to mark not yet available download

--- components/DedServerRow.vue
+++ components/DedServerRow.vue
@@ -14,7 +14,7 @@
         <Badge class="bg-sky-400 text-white" :title="reasonOfDeath">ded</Badge>
       </div>
       <a v-if="downloadPath" class="bg-blue-500 drop-shadow-sm px-3 py-1 rounded text-xl text-white hover:bg-blue-600 hover:drop-shadow-md transition-all" :href="downloadPath">Download Map as ZIP</a>
-      <span v-else class="bg-blue-500 drop-shadow-sm px-3 py-1 rounded text-xl text-white">Download coming soon&trade;</span>
+      <span v-else class="bg-blue-300 drop-shadow-sm px-3 py-1 rounded text-xl text-white">Download coming soon&trade;</span>
     </div>
   </div>
 </template>
