ee0c650609768f0d71b32da1d76333e7169d553d
LinusTebbe <linus@tebbe.dev>
2023-08-28 10:12:22 +0200

    enlarge rows

--- components/DedServerRow.vue
+++ components/DedServerRow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white rounded-md drop-shadow-md flex flex-col md:flex-row items-center px-3 py-0 place-content-between">
+  <div class="bg-white rounded-md drop-shadow-md flex flex-col md:flex-row items-center px-3 py-2 place-content-between">
     <div class="flex flex-row place-content-between w-full md:w-auto">
       <div class="self-center">
         <img
@@ -10,7 +10,7 @@
       </div>
       <h2 class="text-3xl lg:text-3xl xl:text-[50px]">{{name}}</h2>
     </div>
-    <div class="flex flex-row gap-2 py-2 place-content-between w-full md:w-auto">
+    <div class="flex flex-row gap-2  place-content-between w-full md:w-auto">
       <div class="self-center">
         <Badge class="bg-sky-400 text-white" :title="reasonOfDeath">ded</Badge>
       </div>
