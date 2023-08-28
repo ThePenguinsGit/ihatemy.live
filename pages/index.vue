362768bb2042e502a39136b9b72ea3c4e5a98ffb
LinusTebbe <linus@tebbe.dev>
2023-08-28 14:53:00 +0200

    add correct death reasons

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,11 @@
     </div>
     <div class="flex flex-col gap-5 flex-grow">
       <AliveServerRow name="🦀" image-path="/img/crab.png" hostname="crab.ihatemy.live"/>
-      <DedServerRow name="MC Eternal 2" reason-of-death="died of robot uprising" image-path="/img/mce2.png" download-path="https://s3.eu-central-1.wasabisys.com/penguins-maps/maps/mce2.zip" />
-      <DedServerRow name="MC Eternal" reason-of-death="died of ded" image-path="/img/mce.png" :download-path="null"/>
-      <DedServerRow name="SkyFactory 4" reason-of-death="died of ded" image-path="/img/sf4.png" download-path="https://s3.eu-central-1.wasabisys.com/penguins-maps/maps/sf4.zip" />
-      <DedServerRow name="Enigmatica 2: Expert - E2E" reason-of-death="died of ded" image-path="/img/e2e.png" download-path="https://s3.eu-central-1.wasabisys.com/penguins-maps/maps/e2e.zip" />
-      <DedServerRow name="The 1.12.2 Pack" reason-of-death="died of ded" image-path="/img/1122.png" download-path="https://s3.eu-central-1.wasabisys.com/penguins-maps/maps/1122.zip" />
+      <DedServerRow name="MC Eternal 2" reason-of-death="shitted and cummed" image-path="/img/mce2.png" download-path="https://s3.eu-central-1.wasabisys.com/penguins-maps/maps/mce2.zip" />
+      <DedServerRow name="MC Eternal" reason-of-death="died of robot uprising" image-path="/img/mce.png" :download-path="null"/>
+      <DedServerRow name="SkyFactory 4" reason-of-death="died of boredom" image-path="/img/sf4.png" download-path="https://s3.eu-central-1.wasabisys.com/penguins-maps/maps/sf4.zip" />
+      <DedServerRow name="Enigmatica 2: Expert - E2E" reason-of-death="died of a nasty adderall & caffeine overdose" image-path="/img/e2e.png" download-path="https://s3.eu-central-1.wasabisys.com/penguins-maps/maps/e2e.zip" />
+      <DedServerRow name="The 1.12.2 Pack" reason-of-death="sharted itself" image-path="/img/1122.png" download-path="https://s3.eu-central-1.wasabisys.com/penguins-maps/maps/1122.zip" />
     </div>
   </div>
 </template>
