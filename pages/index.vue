e1d4fb4942b92a881b66670802b12f09d9259240
LinusTebbe <linus@tebbe.dev>
2023-08-28 10:28:13 +0200

    fix image link

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="px-5 flex flex-col xl:flex-row gap-4 pt-10">
     <iframe class="hidden xl:block" src="https://discordapp.com/widget?id=637719625274228743&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-    <a class="xl:hidden" href="https://discord.gg/QerZux9gyM">
-      <img src="https://discord.com/api/guilds/637719625274228743/widget.png?style=banner2" alt="Join our Discord"/>
-    </a>
+    <div>
+      <a class="block xl:hidden w-fit" href="https://discord.gg/QerZux9gyM">
+        <img class="text-white" src="https://discord.com/asdaapi/guilds/637719625274228743/widget.png?style=banner2" alt="Join our Discord (p.s. this image can't be shown because it was blocked by your browser)"/>
+      </a>
+    </div>
     <div class="flex flex-col gap-5 flex-grow">
       <AliveServerRow name="🦀" image-path="/img/crab.png" hostname="crab.ihatemy.live"/>
       <DedServerRow name="MC Eternal 2" reason-of-death="died of robot uprising" image-path="/img/mce2.png" download-path="https://s3.eu-central-1.wasabisys.com/penguins-maps/maps/mce2.zip" />
