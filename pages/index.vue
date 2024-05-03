78a649cf7e9ec9bdb697a6679c91e11ef10ca2e2
Linus Tebbe <linus@tebbe.dev>
2024-05-03 14:14:52 +0200

    fix margins

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
     </div>
     <div class="flex flex-col gap-5 flex-grow">
       <AliveServerRow name="All The Mods 9" image-path="/img/atm9.png" hostname="atm9.ihatemy.live"/>
-      <AliveServerRow name="🦀" image-path="/img/crab.png" hostname="crab.ihatemy.live"/>
+      <AliveServerRow name=" 🦀" image-path="/img/crab.png" hostname="crab.ihatemy.live"/>
       <DedServerRow name="MC Eternal 2" reason-of-death="shitted and cummed" image-path="/img/mce2.png" download-path="https://map-storage.ihatemy.live/maps/mce2.zip" />
       <DedServerRow name="MC Eternal" reason-of-death="died of robot uprising" image-path="/img/mce.png" :download-path="null" />
       <DedServerRow name="SkyFactory 4" reason-of-death="died of boredom" image-path="/img/sf4.png" download-path="https://map-storage.ihatemy.live/maps/sf4.zip" />
