bcfd238ec4d071517f7481a673b035f1c91a8d0e
Linus Tebbe <linus@tebbe.dev>
2024-05-06 18:23:46 +0200

    ADD download link

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
       <AliveServerRow name="All The Mods 9" image-path="/img/atm9.png" hostname="atm9.ihatemy.live"/>
       <AliveServerRow name=" 🦀" image-path="/img/crab.png" hostname="crab.ihatemy.live"/>
       <DedServerRow name="MC Eternal 2" reason-of-death="shitted and cummed" image-path="/img/mce2.png" download-path="https://map-storage.ihatemy.live/maps/mce2.zip" />
-      <DedServerRow name="MC Eternal" reason-of-death="died of robot uprising" image-path="/img/mce.png" :download-path="null" />
+      <DedServerRow name="MC Eternal" reason-of-death="died of robot uprising" image-path="/img/mce.png" download-path="https://map-storage.ihatemy.live/maps/mce.zip" />
       <DedServerRow name="SkyFactory 4" reason-of-death="died of boredom" image-path="/img/sf4.png" download-path="https://map-storage.ihatemy.live/maps/sf4.zip" />
       <DedServerRow name="Enigmatica 2: Expert - E2E" reason-of-death="died of a nasty adderall & caffeine overdose" image-path="/img/e2e.png" download-path="https://map-storage.ihatemy.live/maps/e2e.zip" />
       <DedServerRow name="The 1.12.2 Pack" reason-of-death="sharted itself" image-path="/img/1122.png" download-path="https://map-storage.ihatemy.live/maps/1122.zip" />
