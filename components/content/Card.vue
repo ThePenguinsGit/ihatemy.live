8004a39276a70a4822062e79972fa7c2c75e46f6
OzTheM9 <philip@stocksley.dev>
2025-02-21 16:40:02 +0000

    I went ahead and CHAAAAAAAAAAAAAAAANGED some small stuff, ill take a loot at it later again if I get more ideas

--- components/content/Card.vue
+++ components/content/Card.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="card flex-auto">
-    <slot />
+  <div class="card">
+    <div class="card-content">
+      <slot />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card-content {
+    width: 100%;
+}
+</style>
