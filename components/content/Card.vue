08e42a41c89f8ad803efba55762fc8dfe5896b8b
LinusTebbe <linus@tebbe.dev>
2025-02-22 12:09:53 +0100

    FIX card being vertically centered

--- components/content/Card.vue
+++ components/content/Card.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="card">
-    <div class="card-content">
-      <slot />
-    </div>
+    <slot mdc-unwrap="p" />
   </div>
 </template>
 
 <style scoped>
-.card {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.card-content {
-    width: 100%;
-}
 </style>
