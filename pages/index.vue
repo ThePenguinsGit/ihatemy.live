3d8d2daccb87a0e57c1432b96dfc858882c0bd82
LinusTebbe <linus@tebbe.dev>
2026-01-13 00:27:23 +0100

    add atm10tts to index

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,7 @@
         <AliveServerRow name="MC Eternal 2" version="1.2.0.3" image-path="/img/mce2-2.png" hostname="mce2.ihatemy.live" mapUrl='https://atm10.ihatemy.live'/>
         <AliveServerRow name="Society: Sunlit Valley" version="3.4.2" image-path="/img/sv.png" hostname="sv.ihatemy.live" mapUrl='https://atm10.ihatemy.live'/>
         <AliveServerRow name="Prominence 2" version="3.9.14hf" image-path="/img/p2he.png" hostname="p2he.ihatemy.live" mapUrl='https://atm10.ihatemy.live'/>
+        <AliveServerRow name="ATM10: To the Sky" version="1.8" image-path="/img/atm10tts.png" hostname="atm10tts.ihatemy.live" mapUrl='https://atm10.ihatemy.live'/>
         <AliveServerRow name="All The Mods 10" version="5.4" image-path="/img/atm10.png" hostname="atm10.ihatemy.live" mapUrl='https://atm10.ihatemy.live'/>
         <AliveServerRow name="All The Mods 9" version="1.1.1" image-path="/img/atm9.png" hostname="atm9.ihatemy.live" mapUrl='https://atm9.ihatemy.live'/>
         <DedServerRow name=" 🦀" reason-of-death="died of dead" image-path="/img/crab.png" download-path="https://map-storage.ihatemy.live/maps/crab.zip" :content-length-of-download="6248410954" />
