f57c64ecbdc3ea64c25ff8953602d8b9f938b429
LinusTebbe <linus@tebbe.dev>
2026-03-25 19:54:57 +0100

    reorder alive packs

--- pages/index.vue
+++ pages/index.vue
@@ -9,12 +9,12 @@
         </a>
       </div>
       <div class="flex flex-col gap-5 flex-grow">
-        <AliveServerRow name="GregTech: New Horizons" version="2.8.4" image-path="/img/gtnh.png" hostname="gtnh.ihatemy.live" mapUrl='https://maps.ihatemy.live/gtnh'/>
-        <AliveServerRow name="MC Eternal 2" version="1.2.0.3" image-path="/img/mce2-2.png" hostname="mce2.ihatemy.live" mapUrl='https://maps.ihatemy.live/mce2'/>
-        <AliveServerRow name="Society: Sunlit Valley" version="4.0.4" image-path="/img/sv.png" hostname="sv.ihatemy.live" mapUrl='https://maps.ihatemy.live/sv'/>
-        <AliveServerRow name="Prominence 2" version="3.9.21" image-path="/img/p2he.png" hostname="p2he.ihatemy.live" mapUrl='https://maps.ihatemy.live/p2he'/>
         <AliveServerRow name="ATM10: To the Sky" version="2.0.0" image-path="/img/atm10tts.png" hostname="atm10tts.ihatemy.live" mapUrl='https://maps.ihatemy.live/atm10tts'/>
         <AliveServerRow name="All The Mods 10" version="6.1" image-path="/img/atm10.png" hostname="atm10.ihatemy.live" mapUrl='https://maps.ihatemy.live/atm10'/>
+        <AliveServerRow name="GregTech: New Horizons" version="2.8.4" image-path="/img/gtnh.png" hostname="gtnh.ihatemy.live" mapUrl='https://maps.ihatemy.live/gtnh'/>
+        <AliveServerRow name="MC Eternal 2" version="1.2.0.3" image-path="/img/mce2-2.png" hostname="mce2.ihatemy.live" mapUrl='https://maps.ihatemy.live/mce2'/>
+        <AliveServerRow name="Prominence 2" version="3.9.21" image-path="/img/p2he.png" hostname="p2he.ihatemy.live" mapUrl='https://maps.ihatemy.live/p2he'/>
+        <AliveServerRow name="Society: Sunlit Valley" version="4.0.4" image-path="/img/sv.png" hostname="sv.ihatemy.live" mapUrl='https://maps.ihatemy.live/sv'/>
       </div>
     </div>
   </div>
