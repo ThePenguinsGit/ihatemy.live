0dea76f95f2a0f3610f4deef24ae47c4886a041c
LinusTebbe <linus@tebbe.dev>
2026-07-12 02:35:21 +0200

    add loading placeholder to minecraft heads to reduce layout shift

--- components/McHead.vue
+++ components/McHead.vue
@@ -1,12 +1,34 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   uuid: string
   alt?: string
 }>()
+
+const img = ref<HTMLImageElement>()
+const loaded = ref(false)
+
+onMounted(() => {
+  if (img.value?.complete) loaded.value = true
+})
+
+watch(() => props.uuid, () => {
+  loaded.value = false
+})
 </script>
 
 <template>
-  <img :src="`https://crafthead.net/avatar/${uuid}`" :alt="alt" />
+  <div class="relative aspect-square overflow-hidden">
+    <Loading v-if="!loaded" class="absolute inset-0" />
+    <img
+      v-show="loaded"
+      ref="img"
+      :src="`https://crafthead.net/avatar/${uuid}`"
+      :alt="alt"
+      class="w-full h-full"
+      @load="loaded = true"
+      @error="loaded = true"
+    />
+  </div>
 </template>
 
 <style scoped>
